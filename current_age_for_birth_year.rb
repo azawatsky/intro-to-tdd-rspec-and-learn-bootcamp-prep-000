4f5d532c77a4a8efa005414f2c842e39ebdb21f2
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-20 20:58:27 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,3 @@
-def current_age_for_birth_year(current_year,birth_year)
-  puts age_of_person = current_year - birth_year
+def current_age_for_birth_year(birth_year)
+  puts age_of_person = time. - birth_year
 end
